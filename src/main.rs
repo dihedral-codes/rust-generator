06df099139a17839de6484142253e7f544f88c48
Ian McLoughlin <ian.mcloughlin@gmit.ie>
2016-04-22 18:13:55 +0100

    Generates binary strings.

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,49 @@
-struct Squares {
-  curr: u8
+struct Bracelets {
+  curr: u64
+	, mask: u64
 }
 
-impl Iterator for Squares {
-  type Item = u8;
+impl Bracelets {
+	
+	fn new(k: u8) -> Bracelets  {
+		
+		// Set the bit mask for k bits.
+		let mut mask:u64 = 0;
+		let mut i: u8 = k;
+		while i > 4 {
+			mask = (mask << 4) | 0b1111u64;
+			i -= 4;
+		}
+		while i > 0 {
+			mask = (mask << 1) | 0b1u64;
+			i -= 1;
+		}
+		
+		// Let the initial value be 10000..0 with k zeroes.
+		let curr: u64 = 1u64 << k - 1;
+		
+		Bracelets { curr: curr, mask: mask }
+	}
 
-  fn next(&mut self) -> Option<u8> {
+}
 
-    self.curr += 1;
+impl Iterator for Bracelets {
+  type Item = u64;
 
-    if self.curr <= 2u8.pow(4) - 1 {
-      Some(self.curr.pow(2))
+  fn next(&mut self) -> Option<u64> {
+	
+    self.curr += 1u64;
+		
+    if self.curr <= self.mask {
+      Some(self.curr)
     } else {
       None
     }
   }
 }
 
-fn squares() -> Squares {
-  Squares { curr: 0 }
-}
-
-
 fn main() {
-  for i in squares() {
-    println!("{}", i);
+  for i in Bracelets::new(4) {
+    println!("{:b}", i);
   }
 }
