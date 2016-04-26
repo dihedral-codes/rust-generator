c46beb7e81931048c8b45f0bdd1dcb230ef0c1c4
Ian McLoughlin <ian@ianix.iml.com>
2016-04-26 15:00:12 +0100

    Done.

--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,9 @@ impl Bracelets {
 			mask = (mask << 1) | 0b1u64;
 			i -= 1;
 		}
-	
+
+    // Start with 0111..11000....0
     let curr: u64 = ((1u64 << w) - 1) << (k - w - 1);
-		// let curr: u64 = (1u64 << k - 1) | ((1u64 << (w - 1)) - 1); 
 
 		Bracelets { curr: curr, mask: mask }
 	}
@@ -32,6 +32,7 @@ impl Iterator for Bracelets {
 
   fn next(&mut self) -> Option<u64> {
 
+    // Adapted from: http://graphics.stanford.edu/~seander/bithacks.html
     let t:u64 = self.curr | (self.curr - 1);
     self.curr = (t + 1) | (((!t & (t + 1)) - 1) >> (self.curr.trailing_zeros() + 1));  
     
@@ -44,7 +45,9 @@ impl Iterator for Bracelets {
 }
 
 fn main() {
-  for i in Bracelets::new(8, 3) {
-    println!("{:b}", i);
+  for i in 3..15 {
+    for j in Bracelets::new(48, i) {
+      println!("{:b}", j);
+    }
   }
 }
